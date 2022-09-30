2b6269954d00a7e4be44b991f260cb40b7e222bf
Truong <nguyenanhtruong5350@gmail.com>
2022-09-30 13:33:17 +0700

    upload

---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+$('.heal__slider').slick({
+    prevArrow:`<button type='button' class='slick-prev pull-left'><img src="/src/img/heal/slick_slider/arow_left.svg" ></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><img src="/src/img/heal/slick_slider/arrow_right.svg" ></button>`,
+});
+$('.slide-comment').slick({
+    prevArrow:`<button type='button' class='slick-prev pull-left'><img src="/src/img/comment/arrow_left.svg" ></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><img src="/src/img/comment/arrow_right.svg" ></button>`,
+});
