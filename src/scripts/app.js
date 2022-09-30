88dc2bb759f9eb42ec8591d396002abcbd8a8cbb
Truong <nguyenanhtruong5350@gmail.com>
2022-09-30 13:51:12 +0700

    upload

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,8 @@
 $('.heal__slider').slick({
-    prevArrow:`<button type='button' class='slick-prev pull-left'><img src="/src/img/heal/slick_slider/arow_left.svg" ></button>`,
-    nextArrow:`<button type='button' class='slick-next pull-right'><img src="/src/img/heal/slick_slider/arrow_right.svg" ></button>`,
+    prevArrow:`<button type='button' class='slick-prev pull-left'><img src="/src/img/heal/slick_slider/arrow_left.png" ></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><img src="/src/img/heal/slick_slider/arrow_right.png" ></button>`,
 });
 $('.slide-comment').slick({
-    prevArrow:`<button type='button' class='slick-prev pull-left'><img src="/src/img/comment/arrow_left.svg" ></button>`,
-    nextArrow:`<button type='button' class='slick-next pull-right'><img src="/src/img/comment/arrow_right.svg" ></button>`,
+    prevArrow:`<button type='button' class='slick-prev pull-left'><img src="/src/img/comment/arrow_left.png" ></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><img src="/src/img/comment/arrow_right.png" ></button>`,
 });
